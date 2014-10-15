593479b2f38faab9a601f6162318236e1c7e7b47
phi <phi1618jp@gmail.com>
2014-10-15 17:15:46 +0900

    add a command to output a document

--- gruntfile.js
+++ gruntfile.js
@@ -46,6 +46,20 @@ module.exports = function(grunt) {
                 },
             },
         },
+        shell: {
+            docs: {
+                command: 'jsduck ./src --output ./docs --title "high docs"',
+                options: {
+                    stdout: true,
+                    callback: function(err, stdout, stderr, cb) {
+                        console.log(err);
+                        console.log(stdout);
+                        console.log(stderr);
+                        console.log(cb);
+                    },
+                },
+            },
+        }
     });
 
     for (var key in pkg.devDependencies) {
@@ -55,5 +69,6 @@ module.exports = function(grunt) {
     }
 
     grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('docs', ['shell:docs']);
 };
 
